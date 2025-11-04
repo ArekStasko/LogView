9b0631a2bd5a222fd6fb7e88448eaa0de9bb26c5
ArekStasko <arek.stasko@gmail.com>
2025-11-04 20:45:26 +0100

    MainPanel scaffold

--- src/Views/MainPanel.java
+++ src/Views/MainPanel.java
@@ -1,4 +1,19 @@
 package Views;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class MainPanel {
+    JFrame frame;
+
+    public MainPanel() {
+        frame = new JFrame();
+        frame.setTitle("LogView");
+        frame.setSize(800, 600);
+        frame.setLayout(new FlowLayout(FlowLayout.CENTER));
+    }
+
+    private JPanel BuildPanel(){
+        
+    }
 }
