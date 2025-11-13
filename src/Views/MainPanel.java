43758b2303bf83d29ab593724d263818f7bf4826
ArekStasko <arek.stasko@gmail.com>
2025-11-13 23:10:37 +0100

    Correct revalidate and repaint frame

--- src/Views/MainPanel.java
+++ src/Views/MainPanel.java
@@ -33,6 +33,7 @@ public class MainPanel {
                 this.fileService,
                 () -> {
                     this.appList.RenderFiles();
+                    frame.revalidate();
                     frame.repaint();
                 }
         );
